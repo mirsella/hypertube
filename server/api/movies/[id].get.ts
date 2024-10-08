8ade4d4ed5e50351776acba0b9e8240b6b9d680e
mirsella <mirsella@protonmail.com>
2024-10-08 12:15:26 +0200

    start to implement /movies/:id

---
+++ server/api/movies/[id].get.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+  let base64 = getRouterParam(event, "id");
+  if (!base64) throw createError({ statusCode: 400 });
+  const title = Buffer.from(base64, "base64").toString();
+  // look if movie exists in fs, something like ./movies/{base64}.mp4, and send the file
+  // if not, download the torrent from jackett and start download. then response with movie stream
+});
