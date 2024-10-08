d31c64431a2af78069e8a448e6a1c47b0fe1259c
mirsella <mirsella@protonmail.com>
2024-10-08 15:39:23 +0200

    ]add comment about nitro tasks

--- server/api/movies/[id].get.ts
+++ server/api/movies/[id].get.ts
@@ -3,5 +3,6 @@ export default defineEventHandler(async (event) => {
   if (!base64) throw createError({ statusCode: 400 });
   const title = Buffer.from(base64, "base64").toString();
   // look if movie exists in fs, something like ./movies/{base64}.mp4, and send the file
-  // if not, get the the torrent from jackett and start download. then response with movie stream
+  // if not, get the the torrent from jackett and start download, then send the file
+  // we can download the torrent using a nitro tasks https://nitro.unjs.io/guide/tasks
 });
