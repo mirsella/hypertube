c0fa0664a4c3f34ba66b9658b2c191dd46f1552c
mirsella <mirsella@protonmail.com>
2024-10-16 23:32:50 +0200

    add TODO comments with future lib we will use

--- server/api/movies/[id].get.ts
+++ server/api/movies/[id].get.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { Readable } from "node:stream";
 import path from "path";
 import { torrent_client } from "~/server/utils/webtorrent";
 
@@ -31,8 +32,14 @@ export default defineEventHandler(async (event) => {
   const magnet =
     "magnet:?xt=urn:btih:14E16CEA3FC987267A61DC8342E86916CEFCA9D3&dn=Deadpool+and+Wolverine+2024+MA+WEBRip+SDR+10Bit+1440p+DDP5.1+Atmos+HEVC-3Li&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce";
   const torrent = torrent_client.add(magnet);
-  // TODO:
   const torrent_files = torrent.files.sort((a, b) => b.length - a.length);
   const biggest_file_stream = torrent_files[0].createReadStream();
-  return sendStream(event, biggest_file_stream);
+
+  // TODO: download subtitles from opensubtitles
+  // https://github.com/vankasteelj/opensubtitles.com
+
+  // TODO: before returning the stream, we should run it through ffmpeg to convert it to mp4, and add subtitles
+  // github.com/fluent-ffmpeg/node-fluent-ffmpeg
+
+  return sendStream(event, new Readable().wrap(biggest_file_stream));
 });
