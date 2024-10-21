e08ec7d2a79405cc5c77f3870fbabb61155fd5a6
mirsella <mirsella@protonmail.com>
2024-10-21 23:25:06 +0200

    update pages/movies.vue with fixed /movies/[id]/stream

--- pages/movies.vue
+++ pages/movies.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <ClientOnly>
-      <video src="http://localhost:3000/api/movies/arst" controls></video>
+      <!-- "deadpool & wolverine" as base64 -->
+      <video
+        src="http://localhost:3000/api/movies/ZGVhZHBvb2wgJiB3b2x2ZXJpbmUK"
+        controls
+      ></video>
     </ClientOnly>
   </div>
 </template>
