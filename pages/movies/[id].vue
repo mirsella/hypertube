d4d8e4340075478831c017576dfead28e16340bc
mirsella <mirsella@protonmail.com>
2024-10-23 15:28:49 +0200

    create /movies/[id] page

---
+++ pages/movies/[id].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+let id = useRouter().param.id;
+if (id === "default") id = "ZGVhZHBvb2wgJiB3b2x2ZXJpbmUK";
+const title = Buffer.from(id, "base64").toString();
+</script>
+
+<template>
+  <div>
+    {{ title }}
+    <ClientOnly>
+      <video
+        :src="`http://localhost:3000/api/movies/${id}/stream`"
+        controls
+      ></video>
+    </ClientOnly>
+  </div>
+</template>
