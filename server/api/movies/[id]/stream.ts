702f788f55f561cf1157d2f7b16ac9416425cac6
mirsella <mirsella@protonmail.com>
2024-11-10 18:13:46 +0100

    use imdb id in /api/movies/stream

--- server/api/movies/[id]/stream.ts
+++ server/api/movies/[id]/stream.ts
@@ -8,10 +8,8 @@ import WebTorrent from "webtorrent";
 export default defineEventHandler(async (event) => {
   // TODO: check auth
   const moviesDir = useRuntimeConfig().moviesDir;
-  // path.basename to sanitize the input (path traversal attack)
-  const base64 = path.basename(getRouterParam(event, "id") || "");
-  if (!base64) throw createError({ statusCode: 400 });
-  const filepath = path.join(moviesDir, base64);
+  const id = path.basename(getRouterParam(event, "id") || "");
+  const filepath = path.join(moviesDir, id);
 
   const file_stream = await new Promise<fs.ReadStream | null>((resolve) => {
     const stream = fs.createReadStream(filepath);
@@ -24,21 +22,16 @@ export default defineEventHandler(async (event) => {
   if (file_stream) return sendStream(event, file_stream);
 
   const torrent_client = new WebTorrent();
-  const title = Buffer.from(base64, "base64").toString();
-  // TODO: get the magnet/torrent from jackett api instead, this is just for testing
-  const magnet = // deadpool, mkv 1.3gb
-    "magnet:?xt=urn:btih:87A2D22EB879593B48B3D3EE6828F56E2BFB4415&dn=Deadpool.and.Wolverine.2024.1080p.AMZN.WEBRip.1400MB.DD5.1.x264-GalaxyRG&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.birkenwald.de%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fopentor.org%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fuploads.gamecoast.net%3A6969%2Fannounce&tr=https%3A%2F%2Ftracker.foreverpirates.co%3A443%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce";
-  // const magnet = // rick and morty, mkv 156mb
-  //   "magnet:?xt=urn:btih:c4abdb6d30b1cdae74e1237868dc64d47dfcd714&dn=Rick.and.Morty.S04E04.Claw.and.Hoarder.Special.Ricktims.Morty.HDTV.x264-CRiMSON%5BTGx%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.si%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce";
-  // const magnet = // singel, mp4, 133mb
-  //   "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F";
+
+  const infos = await (await fetch(`/api/movies/${id}`)).json();
+  // get magnet
 
   let torrent = torrent_client.torrents.find(
-    (t) => t.magnetURI.toLowerCase() === magnet.toLowerCase(),
+    (t) => t.magnetURI.toLowerCase() === infos.torrents[0].magnet.toLowerCase(),
   );
   if (!torrent) {
-    console.log("starting torrent for", title);
-    torrent = torrent_client.add(magnet);
+    console.log("starting torrent for", infos.title);
+    torrent = torrent_client.add(infos.torrents[0].magnet);
   }
   if (!torrent.ready) {
     await new Promise<void>((resolve) => {
@@ -51,7 +44,9 @@ export default defineEventHandler(async (event) => {
   });
   const biggest_file = biggest_files[0];
   biggest_file.select();
-  console.log(`torrent biggest file for ${title} is ${biggest_file.name}`);
+  console.log(
+    `torrent biggest file for ${infos.title} is ${biggest_file.name}`,
+  );
   // @ts-ignore: webtorrent file implement asyncIterator
   const biggest_file_stream = stream.Readable.from(biggest_file, { end: true });
   const biggest_file_path = torrent.path + "/" + biggest_file.path;
--- server/api/movies/index.ts
+++ server/api/movies/index.ts
@@ -3,9 +3,5 @@ import fs from "fs";
 export default defineEventHandler(async (event) => {
   // TODO: check auth
   const moviesDir = useRuntimeConfig().moviesDir;
-  let promises = fs.readdirSync(moviesDir).map((name) => {
-    let id = name.replace(/\.mp4$/, "");
-    return $fetch(`/api/movies/${id}`);
-  });
-  return Promise.all(promises);
+  return fs.readdirSync(moviesDir);
 });
