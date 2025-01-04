0f4901f56ec365d2f37a10de2df73985eae21446
mirsella <mirsella@protonmail.com>
2025-01-04 17:05:42 +0100

    format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,81 +1,80 @@
 function get_from_env(field: string): string {
   const env = process.env[field];
   if (!env && process.env.NODE_ENV === "development") {
-      console.error(field, "ENV VARIABLE ISN'T SET !!!");
-      process.exit(1);
+    console.error(field, "ENV VARIABLE ISN'T SET !!!");
+    process.exit(1);
   }
   return env || "";
 }
 
 export default defineNuxtConfig({
-compatibilityDate: "2024-04-03",
-devtools: { enabled: true },
-modules: [
-  "@nuxtjs/tailwindcss",
-  "@nuxt/icon",
-  "@sidebase/nuxt-auth",
-  "@nuxtjs/i18n"
-],
-plugins: ["~/plugins/eventBus.ts"],
-runtimeConfig: {
-  debug: true, // Enable debug mode for error logging
-  moviesDir: "./downloaded",
-  opensubtitles_key: get_from_env("OPENSUBTITLES_KEY"),
-  opensubtitles_username: get_from_env("OPENSUBTITLES_USERNAME"),
-  opensubtitles_password: get_from_env("OPENSUBTITLES_PASSWORD"),
-  pgpassword: get_from_env("PGPASSWORD"),
-  tmdbApiKey: get_from_env("TMDB_API_KEY"),
-  jackettApiKey: get_from_env("JACKETT_API_KEY"),
-},
-auth: {
-  isEnabled: true,
-  disableServerSideAuth: false,
-  originEnvKey: "AUTH_ORIGIN",
-  baseURL: "http://localhost:3000/api/auth",
-  sessionRefresh: {
-    enablePeriodically: true,
-    enableOnWindowFocus: true,
-  },
-},
-i18n: {
-  strategy: "prefix_except_default",
-  langDir: "locales/",
-  defaultLocale: "en",
-  locales: [
-    { code: "en", name: "English", language: "en-US", file: "en.json" },
-    { code: "fr", name: "Français", language: "fr-FR", file: "fr.json" },
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "@sidebase/nuxt-auth",
+    "@nuxtjs/i18n",
   ],
-},
-nitro: {
-  experimental: {
-    tasks: true,
-    openAPI: true,
+  plugins: ["~/plugins/eventBus.ts"],
+  runtimeConfig: {
+    debug: true, // Enable debug mode for error logging
+    moviesDir: "./downloaded",
+    opensubtitles_key: get_from_env("OPENSUBTITLES_KEY"),
+    opensubtitles_username: get_from_env("OPENSUBTITLES_USERNAME"),
+    opensubtitles_password: get_from_env("OPENSUBTITLES_PASSWORD"),
+    pgpassword: get_from_env("PGPASSWORD"),
+    tmdbApiKey: get_from_env("TMDB_API_KEY"),
+    jackettApiKey: get_from_env("JACKETT_API_KEY"),
   },
-  openAPI: {
-    production: "prerender",
-    ui: {
-      scalar: {
-        servers: [
-          {
-            url: "http://localhost:3000",
-            description: "dev server",
-          },
-          {
-            url: "https://mirsella.mooo.com/hypertube",
-            description: "Production server",
-          },
-        ],
-      },
+  auth: {
+    isEnabled: true,
+    disableServerSideAuth: false,
+    originEnvKey: "AUTH_ORIGIN",
+    baseURL: "http://localhost:3000/api/auth",
+    sessionRefresh: {
+      enablePeriodically: true,
+      enableOnWindowFocus: true,
     },
   },
-  scheduledTasks: {
-    "0 * * * *": ["remove_old_movies"],
+  i18n: {
+    strategy: "prefix_except_default",
+    langDir: "locales/",
+    defaultLocale: "en",
+    locales: [
+      { code: "en", name: "English", language: "en-US", file: "en.json" },
+      { code: "fr", name: "Français", language: "fr-FR", file: "fr.json" },
+    ],
   },
-  // Fix for file not copied
-},
-hooks: {
-  "dev:reload": () => {
-    require("node-datachannel");
+  nitro: {
+    experimental: {
+      tasks: true,
+      openAPI: true,
+    },
+    openAPI: {
+      production: "prerender",
+      ui: {
+        scalar: {
+          servers: [
+            {
+              url: "http://localhost:3000",
+              description: "dev server",
+            },
+            {
+              url: "https://mirsella.mooo.com/hypertube",
+              description: "Production server",
+            },
+          ],
+        },
+      },
+    },
+    scheduledTasks: {
+      "0 * * * *": ["remove_old_movies"],
+    },
   },
-},
+  hooks: {
+    "dev:reload": () => {
+      require("node-datachannel");
+    },
+  },
 });
--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -77,22 +77,39 @@ async function update_comment(comment_id: string) {
     <span class="loading loading-spinner loading-lg"></span>
   </div>
 
-  <div v-if="infos" class="flex justify-center flex-wrap mx-auto max-w-7xl px-8 space-y-4">
+  <div
+    v-if="infos"
+    class="flex justify-center flex-wrap mx-auto max-w-7xl px-8 space-y-4"
+  >
     {{ infos }}
     <p class="w-full text-center font-bold text-xl">{{ infos?.title }}</p>
     <p v-show="error" class="text-error text-xl font-bold">{{ error }}</p>
     <div class="w-full my-4">
-      <video ref="player" class="rounded-lg shadow-md shadow-primary w-full" controls
-        :src="`/api/movies/${movie_id}/stream`" autoplay :onerror="video_error()">
-        <track v-for="sub in subtitles" kind="subtitles" :label="sub" :srclang="sub"
-          :src="`/api/movies/${movie_id}/subtitles/${sub}`" />
+      <video
+        ref="player"
+        class="rounded-lg shadow-md shadow-primary w-full"
+        controls
+        :src="`/api/movies/${movie_id}/stream`"
+        autoplay
+        :onerror="video_error()"
+      >
+        <track
+          v-for="sub in subtitles"
+          kind="subtitles"
+          :label="sub"
+          :srclang="sub"
+          :src="`/api/movies/${movie_id}/subtitles/${sub}`"
+        />
       </video>
     </div>
 
     <div class="card bg-base-100 shadow-md shadow-secondary w-full">
       <div class="card card-side card-compact card-body">
         <figure>
-          <img :src="`https://image.tmdb.org/t/p/w500${infos.poster_path}`" alt="poster" />
+          <img
+            :src="`https://image.tmdb.org/t/p/w500${infos.poster_path}`"
+            alt="poster"
+          />
         </figure>
         <div class="card-body">
           <p>{{ infos.overview }}</p>
@@ -114,11 +131,17 @@ async function update_comment(comment_id: string) {
         </div>
       </div>
       <div class="flex flex-wrap justify-evenly gap-4 p-4">
-        <div v-for="cast in infos.cast.sort(
-          (a: any, b: any) => b.popularity - a.popularity,
-        )" class="card card-compact min-w-40 w-1/6 bg-base-200">
+        <div
+          v-for="cast in infos.cast.sort(
+            (a: any, b: any) => b.popularity - a.popularity,
+          )"
+          class="card card-compact min-w-40 w-1/6 bg-base-200"
+        >
           <figure>
-            <img :src="`https://media.themoviedb.org/t/p/w500${cast.profile_path}`" alt="poster" />
+            <img
+              :src="`https://media.themoviedb.org/t/p/w500${cast.profile_path}`"
+              alt="poster"
+            />
           </figure>
           <p class="card-title mx-auto mt-1">{{ cast.name }}</p>
           <div class="card-body !pt-2 gap-0">
@@ -135,16 +158,28 @@ async function update_comment(comment_id: string) {
       <div class="card-body">
         <p class="card-title">{{ $t("movies.Comments") }}</p>
         <div class="flex md:flex-nowrap flex-wrap items-end gap-4 mt-2">
-          <textarea v-model="comment_textarea" class="textarea textarea-bordered grow" maxlength="1024"
-            :placeholder="$t('movies.AddAComment')"></textarea>
-          <button class="btn btn-secondary" :class="{ 'btn-disabled': !comment_textarea.length }" @click="send_comment">
+          <textarea
+            v-model="comment_textarea"
+            class="textarea textarea-bordered grow"
+            maxlength="1024"
+            :placeholder="$t('movies.AddAComment')"
+          ></textarea>
+          <button
+            class="btn btn-secondary"
+            :class="{ 'btn-disabled': !comment_textarea.length }"
+            @click="send_comment"
+          >
             post
           </button>
         </div>
         <div class="mr-20">
-          <div v-for="comment of comments" class="chat" :class="[
-            comment.comments.authorId == user?.id ? 'chat-end' : 'chat-start',
-          ]">
+          <div
+            v-for="comment of comments"
+            class="chat"
+            :class="[
+              comment.comments.authorId == user?.id ? 'chat-end' : 'chat-start',
+            ]"
+          >
             <div class="chat-header text-lg ml-1">
               user <b>{{ comment.users?.username || "deleted" }}</b> on
               <ClientOnly>
@@ -154,14 +189,28 @@ async function update_comment(comment_id: string) {
               </ClientOnly>
             </div>
             <div class="chat-bubble chat-bubble-secondary text-lg p-0">
-              <input type="text" class="p-2 outline-none bg-transparent"
-                v-model="comments_input[comment.comments.id]" />
+              <input
+                type="text"
+                class="p-2 outline-none bg-transparent"
+                v-model="comments_input[comment.comments.id]"
+              />
             </div>
-            <div v-if="comment.comments.authorId == user?.id" class="chat-image">
-              <button v-show="comments_input[comment.comments.id] !==
-                comment.comments.content
-                " class="btn-success btn i-carbon-save" @click="update_comment(comment.comments.id)"></button>
-              <button @click="delete_comment(comment.comments.id)" class="btn-error btn i-carbon-trash-can"></button>
+            <div
+              v-if="comment.comments.authorId == user?.id"
+              class="chat-image"
+            >
+              <button
+                v-show="
+                  comments_input[comment.comments.id] !==
+                  comment.comments.content
+                "
+                class="btn-success btn i-carbon-save"
+                @click="update_comment(comment.comments.id)"
+              ></button>
+              <button
+                @click="delete_comment(comment.comments.id)"
+                class="btn-error btn i-carbon-trash-can"
+              ></button>
             </div>
           </div>
         </div>
