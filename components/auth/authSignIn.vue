c8f69c9fb84c30678e2c2040d0359dccd721e7ae
mirsella <mirsella@protonmail.com>
2025-01-05 13:28:57 +0100

    authsignin component: remove min-h-screen

--- components/auth/authSignIn.vue
+++ components/auth/authSignIn.vue
@@ -7,9 +7,7 @@
         </div>
     </div>
     <div v-else> -->
-  <div
-    class="flex min-h-screen flex-col justify-center items-center px-6 py-12 lg:px-8"
-  >
+  <div class="flex flex-col justify-center items-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <h2
         class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white"
