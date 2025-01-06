3ce6d3f315e478b9d127e61d69541e5d4aad7888
mirsella <mirsella@protonmail.com>
2025-01-06 19:36:15 +0100

    fix users patch

--- server/api/users/index.patch.ts
+++ server/api/users/index.patch.ts
@@ -8,23 +8,33 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const headers = getRequestHeaders(event) as HeadersInit;
 
-  if (body.username) {
+  if (body?.username) {
     await $fetch("/api/users/modify/username", {
       headers,
       body,
+      method: "POST",
     });
   }
-  if (body.lastname && body.firstname) {
+  if (body?.lastname && body?.firstname) {
     await $fetch("/api/users/modify/name", {
       headers,
       body,
+      method: "POST",
     });
   }
-  if (body.picture_profil) {
-    await $fetch("/api/users/modify/username", { headers, body });
+  if (body?.picture_profil) {
+    await $fetch("/api/users/modify/username", {
+      headers,
+      body,
+      method: "POST",
+    });
   }
-  if (body.password) {
-    await $fetch("/api/users/modify/password", { headers, body });
+  if (body?.password) {
+    await $fetch("/api/users/modify/password", {
+      headers,
+      body,
+      method: "POST",
+    });
   }
   return { message: "Data has been updated", status: 200 };
 });
